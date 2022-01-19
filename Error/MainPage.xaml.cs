41f97f44c44ad06cefedab5eb962348fd0ed3baa
eduardoagr <egomezr@outlook.com>
2022-01-19 18:53:58 +0100

    Add project files.

---
+++ Error/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿
+using Xamarin.Forms;
+
+namespace Error {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Error/ViewModels/MainVm.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace Error.ViewModels {
+    public class MainVm {
+
+        public ICommand Nav { get; set; }
+
+        public MainVm() {
+
+            Nav = new Command(NavAsync);
+        }
+
+        private async void NavAsync() {
+            await App.Current.MainPage.Navigation.PushAsync(new Page1());
+        }
+    }
+}
